7c3bc1eb8db9bf5e76da7073246b0978fa402264
Juan Gabriel Santa Cruz <juanga_93@hotmail.com>
2021-09-12 23:17:34 -0300

    agrego clases para observer

---
+++ Framework.D-2015/Framework.D-2015/Observer/IObservado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.D_2015.Observer
+{
+    public interface IObservado
+    {
+        List<IObservador> ObservadoresRegistrados { get; }
+
+        void RegistrarObservador(IObservador pObservador);
+        void DesregistrarObservador(IObservador pObservador);
+        void ActualizarObservadores();
+    }
+}
---
+++ Framework.D-2015/Framework.D-2015/Observer/IObservador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Framework.D_2015.Observer
+{
+    public interface IObservador
+    {
+        void Actualizar(Control pObservador);
+    }
+}
